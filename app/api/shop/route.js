2020bd0ed10a39d89ac7fb487aba3e5f54efaa3d
Mahesh <uma@gmail.com>
2024-03-08 21:22:59 +0530

    ok

--- app/api/shop/route.js
+++ app/api/shop/route.js
@@ -1,6 +1,17 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://qawqqahmcrojoxlobjbv.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhd3FxYWhtY3Jvam94bG9iamJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4ODM3NDQsImV4cCI6MjAyNTQ1OTc0NH0.3AMegObHIpOK-37jd9gfCdddXeGdOuWYZ1-APhj00t0'
+const supabase = createClient(supabaseUrl, supabaseKey)
 import { NextResponse } from "next/server"
 
-export const GET = ()=>{
+export const GET = async(req,res)=>{
 
-    return NextResponse.json({msg:102})
+
+    const {data,error} = await supabase.from('Shop').select('*,Users(*)')
+
+    if(error)
+    {
+        return NextResponse.json({msg:error},{status:500})
+    }
+    return NextResponse.json({data},{status:200})
 }
